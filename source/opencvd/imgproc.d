9af75a84a8a2b9068e9138e828e5b01a4c65cc76
Freddie Honohan <honohanf@gmail.com>
2019-05-22 21:13:04 +0100

    Simply added the integer values to the ThresholdType enum to allow for combinations e.g: 'THRESH_BINARY | THRESH_OTSU'.

--- source/opencvd/imgproc.d
+++ source/opencvd/imgproc.d
@@ -514,14 +514,14 @@ void houghLinesPointSet(Mat points, Mat lines, int lines_max, int threshold,
 }
 
 enum: int {
-    THRESH_BINARY,
-    THRESH_BINARY_INV,
-    THRESH_TRUNC,
-    THRESH_TOZERO,
-    THRESH_TOZERO_INV,
-    THRESH_MASK,
-    THRESH_OTSU,
-    THRESH_TRIANGLE
+  THRESH_BINARY = 0,
+  THRESH_BINARY_INV = 1,
+  THRESH_TRUNC = 2,
+  THRESH_TOZERO = 3,
+  THRESH_TOZERO_INV = 4,
+  THRESH_MASK = 7,
+  THRESH_OTSU = 8,
+  THRESH_TRIANGLE = 16
 }
   
 enum: int {
